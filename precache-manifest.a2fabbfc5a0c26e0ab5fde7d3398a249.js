952f2a2bb82dc85a4fc9eacfee93438de36e1c02
evancchen <evancchen2009@gmail.com>
2020-08-03 10:40:18 -0700

    Updates

---
+++ precache-manifest.a2fabbfc5a0c26e0ab5fde7d3398a249.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bd7237d289d1c8875c064060293dba",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "c700a541d818943c6e48",
+    "url": "/pokemon/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9a372b8542975fd7090a",
+    "url": "/pokemon/static/css/main.8599b641.chunk.css"
+  },
+  {
+    "revision": "c700a541d818943c6e48",
+    "url": "/pokemon/static/js/2.1279b723.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon/static/js/2.1279b723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a372b8542975fd7090a",
+    "url": "/pokemon/static/js/main.f863e881.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pokemon/static/media/SEGA.12a3db8b.woff"
+  }
+]);
